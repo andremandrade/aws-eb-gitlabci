e592d3000c9440d46b4e200a9bb2a918154a6116
Andre Meireles <andre.andrade@virtus.ufcg.edu.br>
2021-08-04 15:25:54 -0300

    eb tests

--- client-angular/src/app/app.component.ts
+++ client-angular/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'client-angular-test-v3';
+  title = 'client-angular-test-v2';
 }
